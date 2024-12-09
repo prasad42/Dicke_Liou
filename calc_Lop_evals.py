f80f238b8c49638c7d265c639e9e013af3525854
prasad42 <prasadpawar42@protonmail.com>
2024-12-09 13:54:42 +0530

    Initial commit

---
+++ calc_Lop_evals.py
@@ -0,0 +1,13 @@
+from parameters import *
+import numpy as np
+from scipy.sparse import csc_matrix
+import scipy.sparse.linalg as sla
+import os
+from tqdm import tqdm
+import qutip as qt
+import scipy.linalg as sl
+from dicke_Liou_lib import *
+
+
+for g in g_arr:
+    Dicke_Lop_even_evals_fun(ω, ω0, g, M, j, γ)
---
+++ parameters.py
@@ -0,0 +1,30 @@
+import numpy as np
+'''
+-------------------------------------------------------------------------------------
+				        # parameters #
+-------------------------------------------------------------------------------------
+'''
+
+# SET UP THE CALCULATION
+
+ω  = 1.0; ω0 = 1.0; j = 10; M = 30; v = 30; γ=1.0; β=0; ntraj=100; gc={np.round((np.sqrt(ω*ω0)/2),2)}
+g_arr = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
+
+# Number of random matrices to average over in GOE and GUE
+num_realizations = 1000
+
+# Number of Processes
+nproc = 8
+
+# Time list
+t_vals_0_to_01 = np.linspace(0, 0.1, 1000, endpoint=False)
+t_vals_01_to_1 = np.linspace(0.1, 1, 1000, endpoint=False)
+t_vals_1_to_10 = np.linspace(1, 10, 1000, endpoint=False)
+t_vals_10_to_100 = np.linspace(10, 100, 1000, endpoint=False)
+t_vals_100_to_1000 = np.linspace(100, 1000, 1000)
+tlist = np.concatenate([t_vals_0_to_01, t_vals_01_to_1, t_vals_1_to_10, t_vals_10_to_100, t_vals_100_to_1000])
+
+# Time list for open model with MCWF method
+StartTime = 0
+LateTime = 100
+tlist_open = np.arange(StartTime, LateTime, 0.01)
