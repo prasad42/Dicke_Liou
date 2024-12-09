b7698c0902e77cd401bc1357b719fad68ef0ef58
prasad42 <prasadpawar42@protonmail.com>
2024-12-10 02:20:34 +0530

    Initial commit

--- calc_Lop_evals.py
+++ calc_Lop_evals.py
@@ -2,7 +2,7 @@ from parameters import *
 from dicke_Liou_lib import *
 import matplotlib.pyplot as plt
 
-for g in g_arr:
+for g in tqdm(g_arr):
     eigvals = Dicke_Lop_even_evals_fun(ω, ω0, g, M, j, γ)
 
 plt.plot(eigvals.imag)
